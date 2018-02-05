6a39a8bc7a1fd9e2fd693cb3428a5f8b0c1b29c7
UserUnavailable <klebersilvanaruto6@gmail.com>
2018-02-05 10:10:27 -0200

    BOT

--- userbot.sh
+++ userbot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 TOKEN="<TOKEN>"
-CHAT="<GROUP>" 
 BOT="https://api.telegram.org/bot${TOKEN}"
+MessageID=0
 
 getUpdate(){
 	command=$(curl -s "${BOT}/getUpdates" | wc -l)
@@ -15,32 +15,37 @@ getUpdate(){
 }
 
 sendMessage(){
-	curl -s "${BOT}/sendMessage?chat_id=${CHAT}&parse_mode=Markdown" --data-urlencode "text=$1"
+	curl -s "${BOT}/sendMessage?chat_id=$3&parse_mode=Markdown" --data-urlencode "text=$1" &> /dev/null
 }
 
 replyMessage(){
-	curl -s "${BOT}/sendMessage?chat_id=${CHAT}&reply_to_message_id=$2&parse_mode=Markdown" --data-urlencode "text=$1"
+	curl -s "${BOT}/sendMessage?chat_id=$3&reply_to_message_id=$2&parse_mode=Markdown" --data-urlencode "text=$1" &> /dev/null
 }
 
-MessageID="0"
-while true
-do
+getInfo(){
 	Update=$(getUpdate)
-	
+
 	LastMessageTEXT=$(echo $Update | jq -r '.result[-1].message.text')
 	LastMessageID=$(echo $Update | jq -r '.result[-1].message.message_id')
 	LastMessageUSERNAME=$(echo $Update | jq -r '.result[-1].message.from.username')
+	
+	ChatID=$(echo $Update | jq -r '.result[-1].message.chat.id')
+}
+
+while true
+do
+	getInfo
 
 	if [[ $LastMessageTEXT =~ (/teste(@Raqui333Bot)?) && $LastMessageID != $MessageID ]]
 	then
 		MessageID=$LastMessageID
-		replyMessage "isso é um teste, noob" $MessageID &> /dev/null
+		replyMessage "isso é um teste, noob" $MessageID $ChatID
 	fi
 
 	if [[ $LastMessageUSERNAME = "null"  && $LastMessageID != $MessageID ]]
 	then
 		MessageID=$LastMessageID
-		replyMessage "Coloca um [username](tg://user?id=372539286) ai, noob" $MessageID &> /dev/null
+		replyMessage "Coloca um [username](tg://user?id=372539286) ai, noob" $MessageID $ChatID
 	fi
 
 	sleep 1
