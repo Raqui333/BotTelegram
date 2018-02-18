204445556b254e1a798aee8311903c888ab2b443
UserUnavailable <klebersilvanaruto6@gmail.com>
2018-02-17 23:26:43 -0300

    BOT

--- userbot.py
+++ userbot.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3.5
 
-# python bot test
-
+## import modules
 import json
 import requests
 import time
 
+## Variable default
 TOKEN = "<TOKEN>"
 BOT = "https://api.telegram.org/bot" + TOKEN
 MessageID = 0
@@ -19,12 +19,44 @@ def getUpdates():
     else:
         return requests.get(BOT + "/getUpdates").text
 
-def sendMessage(ChatID, Text):
-    MESSAGE = {"chat_id":ChatID,
-               "text":Text,
-               "parse_mode":"Markdown",
-               "disable_web_page_preview":True}
-    requests.post(BOT + "/sendMessage", data=MESSAGE)
+class msg:
+    ## sendMessage
+    ## Usage: msg.send(chat_id, text)
+    def send(ChatID, Text):
+        MESSAGE = {"chat_id":ChatID,
+                   "text":Text,
+                   "parse_mode":"Markdown",
+                   "disable_web_page_preview":True}
+        requests.post(BOT + "/sendMessage", data=MESSAGE)
+    
+    ## sendMessage with Reply
+    ## Usage: msg.reply(chat_id, text, reply_to_message_id)
+    def reply(ChatID, Text, ReplyID):
+        MESSAGE = {"chat_id":ChatID,
+                   "text":Text,
+                   "parse_mode":"Markdown",
+                   "disable_web_page_preview":True,
+                   "reply_to_message_id":ReplyID}
+        requests.post(BOT + "/sendMessage", data=MESSAGE)
+
+    ## sendMessage with Button
+    ## Usage: msg.button(chat_id, text, reply_markup)
+    def button(ChatID, Text, Button):
+        BUTTON = '{"inline_keyboard":' + Button + '}'
+        MESSAGE = {"chat_id":ChatID,
+                   "text":Text,
+                   "parse_mode":"Markdown",
+                   "disable_web_page_preview":True,
+                   "reply_markup":BUTTON}
+        requests.post(BOT + "/sendMessage", data=MESSAGE)
+
+class edit:
+    def button(ChatID, ID, Button):
+        BUTTON = '{"inline_keyboard":' + Button + '}'
+        MESSAGE = {"chat_id":ChatID,
+                   "message_id":ID,
+                   "reply_markup":BUTTON}
+        requests.post(BOT + "/sendMessage", data=MESSAGE)
 
 def getInfo():
     Update = json.loads(getUpdates())
@@ -41,13 +73,68 @@ def getInfo():
     try: ID = Update["result"][-1]["message"]["message_id"]
     except KeyError: ID = "None"
 
-    return Text, Type, ChatID, ID
+    try: Username = Update["result"][-1]["message"]["from"]["username"]
+    except KeyError: Username = "None"
+
+    try: FName = Update["result"][-1]["message"]["from"]["first_name"]
+    except KeyError: FName = "None"
+
+    try: LName = Update["result"][-1]["message"]["from"]["last_name"]
+    except KeyError: LName = "None"
+
+    try: callbackMID = Update["result"][-1]["callback_query"]["message"]["message_id"]
+    except KeyError: callbackMID = "None"
+
+    try: callbackID = Update["result"][-1]["callback_query"]["id"]
+    except KeyError: callbackID = "None"
+
+    try: callbackDATA = Update["result"][-1]["callback_query"]["data"]
+    except KeyError: callbackDATA = "None"
+
+    return Text, Type, ChatID, ID, Username, FName, LName, callbackMID, callbackID, callbackDATA
+
+## Buttons
+sourceButton = '[[{"text":"Source Code","url":"https://github.com/UserUnavailable/ShellBot/blob/master/userbot.py"}, \
+                  {"text":"GitHub","url":"https://github.com/UserUnavailable"}]]'
+
+repoButton='[[{"text":"CLICK HERE","url":"t.me/RepoMatrix"}, \
+              {"text":"ADMs","callback_data":"ADMs"}]]'
+
+admsButton='[[{"text":"UDglad Dahaka","url":"t.me/Raqui333"}, \
+              {"text":"CMwise","url":"t.me/CMAngel"}, \
+              {"text":"Anaboth Hekmatyar","url":"t.me/Anaboth"}], \
+	     [{"text":"BACK","callback_data":"BACK"}]]'
+
 
 while True:
-    Text, Type, ChatID, ID = getInfo()
+    Text, Type, ChatID, ID, Username, FName, LName, callbackMID, callbackID, callbackDATA = getInfo()
     if ID != MessageID:
         if Text == "/teste":
             MessageID = ID
-            sendMessage(ChatID, "Teste Noob")
+            msg.send(ChatID, "Teste Noob")
+
+        if Username == "None":
+            MessageID = ID
+            msg.reply(ChatID, "Coloca um [username](t.me/Raqui333Bot) otário", ID)
+
+        if Text == "/start" and Type == "private":
+            MessageID = ID
+
+            MSG = "*Olá* _" + FName + " " + LName.replace("None", "") + "_, @" + Username + "\n" \
+                  "esse bot foi feito em `Python` por @Raqui333" + "\n" \
+                  "para saber mais fale com ele no PV"
+
+            msg.button(ChatID, MSG, sourceButton)
+        
+        ## Fazer DPS
+        """
+        if Text == "/repo":
+            MessageID = ID
+            
+            MSG
+             
+            msg.button(ChatID, )
+        """
+    
     # sleep to not explode
     time.sleep(1)
