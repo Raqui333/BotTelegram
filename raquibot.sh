a061f4ec959020fd1cda869d101dcd09f433a42d
UserUnavailable <klebersilva2025@gmail.com>
2019-07-31 12:36:16 -0300

    added bot

--- raquibot.sh
+++ raquibot.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 
-TOKEN="TOKEN"
+TOKEN="<TOKEN>"
 BOT="https://api.telegram.org/bot${TOKEN}"
 
 declare -g DATA
-declare -g ONLY
+declare -g READ
+
+function set_as_read() {
+	READ=$(jq -r '.result[-1].message.message_id' <<< ${DATA})
+}
 
 function get_updates() {
 	DATA=$(curl -s -X GET ${BOT}/getUpdates)
-	if [[ $(wc -l <<< ${DATA}) -eq 100 ]]; then
+	if [[ $(wc -l <<< ${DATA}) -ge 100 ]]; then
 		OFFSET=$(jq -r '.result[-1].update_id' <<< ${DATA})
-		curl -s -d "offset=$(($OFFSET + 1))" -X POST ${BOT}/getUpdates -o /dev/null
+		curl -s -d "offset=$((${OFFSET} + 1))" -X POST ${BOT}/getUpdates -o /dev/null
 	fi
 }
 
 function send_msg() {
 	curl -s -d "chat_id=${1}&text=${2}" -X POST ${BOT}/sendMessage -o /dev/null
-	ONLY=$(jq -r '.result[-1].message.message_id' <<< ${DATA})
+	set_as_read
 }
 
-while :; do
-	get_updates
+function send_sticker() {
+	curl -s -d "chat_id=${1}&sticker=${2}" -X POST ${BOT}/sendSticker -o /dev/null
+	set_as_read
+}
 
+while get_updates ; do
 	MSG=$(jq -r '.result[-1].message.text' <<< ${DATA})
 	CHAT=$(jq -r '.result[-1].message.chat.id' <<< ${DATA})
-	
 	ID=$(jq -r '.result[-1].message.message_id' <<< ${DATA})
 
-	if [[ $ID != $ONLY ]]
-	then
+	if [[ ${ID} != ${READ} ]]; then
 		## /base64 command
-		if [[ ${MSG} =~ ^/base64(@Raqui333bot)? ]]
-		then
+		if [[ ${MSG} =~ ^/base64(@Raqui333bot)? ]]; then
 			MSG=$(sed -E 's:^/base64(@Raqui333bot)?\s::' <<< ${MSG} | tr -d '\n' | base64)
 			send_msg ${CHAT} ${MSG}
+		fi
+		
+		## KappaPride
+		if [[ ${MSG} =~ KappaPride ]]; then
+			send_sticker ${CHAT} "CAADAQADJQcAAsTJswNX-kZLiMEjjRYE"
 		fi
 	fi
 
